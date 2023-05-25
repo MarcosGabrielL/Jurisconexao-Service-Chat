c3bf99412b873abac49a4b3ccec5f56e74f4bd01
Marcos Gabriel Lopes <marcos.lopes@cooperflora.com.br>
2023-05-24 23:24:22 -0300

    update Entity Message

--- src/main/java/br/com/jurisconexao/chat/models/Message.java
+++ src/main/java/br/com/jurisconexao/chat/models/Message.java
@@ -2,13 +2,20 @@ package br.com.jurisconexao.chat.models;
 
 
 import lombok.*;
-
+import jakarta.persistence.*;
 import java.util.Date;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+
+@Entity
+@Table(name = "Message")
 public class Message {
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
     private String senderName;
     private String receiverName;
     private String message;
@@ -43,6 +50,12 @@ public class Message {
 	}
 	public void setStatus(Status status) {
 		this.status = status;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 
